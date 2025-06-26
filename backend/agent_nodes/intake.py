d8451e1afa8450ebd1e79da66f0b47ea68f45d5c
arindakhill <arinda.hillary@gmail.com>
2025-06-25 20:40:03 -0500

    fix requirements.txt and .gitignore

--- backend/agent_nodes/intake.py
+++ backend/agent_nodes/intake.py
@@ -9,15 +9,19 @@ def extract_country(text: str) -> str:
     doc = nlp(text)
     for ent in doc.ents:
         if ent.label_ == "GPE":  # Geo-Political Entity (country, city, state)
-            return ent.text
+            country = ent.text.strip()
+            if country.lower().startswith("the "):
+                country = country[4:]
+            return country
     return None
 
 def intake_node(state: ImmigrationState) -> dict:
     user_question = state.get("user_question", "")
     visa_type = None
-    visa_match = re.search(r"\b(F-1|J-1|B-2|H-1B|CPT|OPT|STEM OPT|H-4|L-1|EB-\d)\b", user_question, re.IGNORECASE)
-    if visa_match:
-        visa_type = visa_match.group(1).upper()
+    visa_matches = re.findall(r"\b(F-1|J-1|B-2|H-1B|CPT|OPT|STEM OPT|H-4|L-1|EB-\d)\b", user_question, re.IGNORECASE)
+    if visa_matches:
+        #visa_type = visa_match.group(1).upper()
+        visa_type = visa_matches[-1].upper()  # Get the last match for more specific visa types
     country = state.get("country") or extract_country(user_question)
     return {
         "user_question": user_question,
